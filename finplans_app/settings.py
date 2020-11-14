f048b9ee1e6d51f9f1e49e147eff95a241eb821e
niqart <nlaraya@uc.cl>
2020-11-14 15:04:34 -0300

    🚀 heroku deploy config

--- finplans_app/settings.py
+++ finplans_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
@@ -126,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
