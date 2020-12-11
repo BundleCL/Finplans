7f8aa958d317e9dd108e42323fd21dfe8dcb027d
niqart <nlaraya@uc.cl>
2020-12-11 20:15:09 -0300

    🚧 mail tests

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
+    path('test_mail', views.test_mail, name='test_mail'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
-from .forms import SignUpForm
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from .forms import SignUpForm
 
 
 def index(request):
@@ -24,5 +26,18 @@ def register(request):
             last_name = form.cleaned_data.get('last_name')
             user = User.objects.create_user(
                 username=email, email=email, password=password, first_name=first_name, last_name=last_name)
-            return redirect('index')
+            return redirect("https://docs.google.com/forms/d/e/1FAIpQLSfTVN6PxiaZkTKmPmZdFL86Hb_Tril-_pEBtM2gV5SePkKRMg/viewform?usp=sf_link")
     return render(request, 'register.html', {'form': form})
+
+
+def test_mail(request):
+    msg = "encriptado"
+    mails = ['email@example.com']  # cambiar
+    send_mail(
+        'Asunto',
+        f'Aquí va el mensaje {msg}',
+        'contacto.finplans@gmail.com',
+        mails,
+        fail_silently=False,
+    )
+    return redirect('index')
